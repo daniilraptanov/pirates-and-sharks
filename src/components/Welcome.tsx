f2f753fe9cc73619830204458c71903dc33d6f75
Alex <oleksii.protopopov@nure.ua>
2024-04-01 22:47:55 +0300

    feat-2: AuthModal

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,16 +1,22 @@
-import { FC, useState } from "react";
-import RegistrationModal from "./RegistrationModal";
-import LoginModal from "./LoginModal";
+import  { FC, useState } from "react";
+import AuthModal from "./AuthModal";
 
 interface WelcomeProps {}
 
-const Welcome: FC<WelcomeProps> = (props) => {
-    const [onLogin, setOnLogin] = useState(false);
-    return onLogin ? (
-        <RegistrationModal />
-    ) : (
-      <LoginModal />
+const Welcome: FC<WelcomeProps> = () => {
+    const [showAuth, setShowAuth] = useState(false);
+
+    const handleToggleAuth = () => {
+        setShowAuth(!showAuth);
+    };
+
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <button onClick={handleToggleAuth}>Open Auth Modal</button>
+            {showAuth && <AuthModal onClose={handleToggleAuth} />}
+        </div>
     );
-}
+};
 
 export default Welcome;
