eeba1157113ceafbbf12e8c18a13774d8bec1515
Alex <oleksii.protopopov@nure.ua>
2024-04-10 16:34:14 +0300

    fix-1: Inventory bar dynamic position

--- src/game/scenes/Inventory.ts
+++ src/game/scenes/Inventory.ts
@@ -12,51 +12,52 @@ export class Inventory extends Scene {
     }
 
     create() {
-        const cameraWidth = this.cameras.main.width;
-        const cameraHeight = this.cameras.main.height;
-        const inventoryHeight = 200;
-        const numRows = 3;
-        const numCols = 5;
-        const cellWidth = cameraWidth / numCols;
-        const cellHeight = inventoryHeight / numRows;
+        const canvasWidth = this.sys.game.canvas.width;
+        const canvasHeight = this.sys.game.canvas.height;
+        const inventoryWidth = 1000;
+        const inventoryHeight = 180;
         const PlayerColor = 0xffe200;
     
+        // Позиция прямоугольника инвентаря
+        const inventoryX = (canvasWidth - inventoryWidth) / 2; // Вычисляем X-координату для центрирования
+        const inventoryY = canvasHeight - inventoryHeight; // Фиксированная Y-координата, чтобы прямоугольник находился внизу
+    
         // Создаем прямоугольник инвентаря
-        const inventoryRect = this.add.rectangle(0, cameraHeight - inventoryHeight, cameraWidth, inventoryHeight, PlayerColor);
+        const inventoryRect = this.add.rectangle(inventoryX, inventoryY, inventoryWidth, inventoryHeight, PlayerColor);
         inventoryRect.setOrigin(0);
         inventoryRect.setAlpha(0.3);
     
         // Рисуем горизонтальные линии таблицы
-        for (let i = 1; i < numRows; i++) {
-            const y = cameraHeight - inventoryHeight + i * cellHeight;
+        const cellWidth = inventoryWidth / 5;
+        const cellHeight = inventoryHeight / 3;
+        for (let i = 1; i < 3; i++) {
+            const y = inventoryY + i * cellHeight;
             const line = this.add.graphics();
             line.lineStyle(2, PlayerColor);
             line.beginPath();
             if (i == 1) {
-                line.moveTo(0, y);
-                line.lineTo(cameraWidth/5*4, y);
-            }
-            else {
-                line.moveTo(cameraWidth/5*3, y);
-                line.lineTo(cameraWidth/5*4, y);
+                line.moveTo(inventoryX, y);
+                line.lineTo(inventoryX + (cellWidth * 4), y);
+            } else {
+                line.moveTo(inventoryX + (cellWidth * 3), y);
+                line.lineTo(inventoryX + (cellWidth * 4), y);
             }
             line.closePath();
             line.strokePath();
         }
     
         // Рисуем вертикальные линии таблицы
-        for (let i = 1; i < numCols; i++) {
-            const x = i * cellWidth;
+        for (let i = 1; i < 5; i++) {
+            const x = inventoryX + i * cellWidth;
             const line = this.add.graphics();
             line.lineStyle(2, PlayerColor);
             line.beginPath();
             if (i != 4) {
-                line.moveTo(x, cameraHeight - (inventoryHeight/3*2)+1);
+                line.moveTo(x, inventoryY + cellHeight);
+            } else {
+                line.moveTo(x, inventoryY);
             }
-            else {
-                line.moveTo(x, cameraHeight -inventoryHeight);
-            }
-            line.lineTo(x, cameraHeight);
+            line.lineTo(x, inventoryY + inventoryHeight);
             line.closePath();
             line.strokePath();
         }
@@ -66,7 +67,7 @@ export class Inventory extends Scene {
         const borderColor = PlayerColor;
         const borderRect = this.add.graphics();
         borderRect.lineStyle(borderThickness, borderColor);
-        borderRect.strokeRect(0, cameraHeight - inventoryHeight, cameraWidth, inventoryHeight);
+        borderRect.strokeRect(inventoryX, inventoryY, inventoryWidth, inventoryHeight);
     
         EventBus.emit('current-scene-ready', this);
 
