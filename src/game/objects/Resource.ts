e000072c5cff0a0258b0f2b565fcd0d574749182
Alex <oleksii.protopopov@nure.ua>
2024-04-17 00:39:26 +0300

    feat-1: Resource class

--- src/game/objects/Resource.ts
+++ src/game/objects/Resource.ts
@@ -2,39 +2,116 @@ import { ResourceType } from "../../enums/resource-type";
 
 export class Resource {
     private scene: Phaser.Scene;
-    private selected: boolean;
-
+    private image:Phaser.GameObjects.Image;
+    selected: boolean;
+    SomeSelected: boolean;
     resourceType: ResourceType;
-    hotkey: number;
+    hotkey: string;
 
     fullSizeX: number;
     fullSizeY: number;
 
-    fullSizeOffsetX: number;
-    fullSizeOffsetY: number;
     fullSizeOffsetWidth: number;
+    fullSizeOffsetHeight: number;
 
-    selectedX: number;
-    selectedY: number;
+    SelectedSizeX: number;
+    SelectedSizeY: number;
 
     constructor(
         scene: Phaser.Scene,
         resourceType: ResourceType, 
-        hotkey: number,
-        fullSizeOffsetX: number,
-        fullSizeOffsetY: number,
+        hotkey: string,
+        fullSizeX: number,
+        fullSizeY: number,
         fullSizeOffsetWidth: number,
+        fullSizeOffsetHeight: number,
+        SelectedSizeX: number,
+        SelectedSizeY: number,
+        
     ) {
         this.scene = scene;
         this.resourceType = resourceType;
         this.hotkey = hotkey;
-        this.fullSizeOffsetX = fullSizeOffsetX;
-        this.fullSizeOffsetY = fullSizeOffsetY;
+        this.fullSizeX = fullSizeX;
+        this.fullSizeY = fullSizeY;
         this.fullSizeOffsetWidth = fullSizeOffsetWidth;
+        this.fullSizeOffsetHeight = fullSizeOffsetHeight;
+        this.SelectedSizeX = SelectedSizeX;
+        this.SelectedSizeY = SelectedSizeY;
+        this.handleClick = this.handleClick.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.MouseOver = this.MouseOver.bind(this);
+        this.MouseOut = this.MouseOut.bind(this);
+        this.selected = false;
+        this.SomeSelected = false;
     }
 
-    initialize(x: number, y: number) {
+    initialize(image:string) {
+        this.image = this.scene.add.image(this.fullSizeX, this.fullSizeY, image);
+        this.image.setOrigin(0);
+        this.image.setInteractive();
+        this.image.on('pointerdown', this.handleClick);
+        this.image.on('pointerover', this.MouseOver);
+        this.image.on('pointerout', this.MouseOut);
+        if(this.scene.input.keyboard){
+        this.scene.input.keyboard.on('keydown', this.handleKeyDown);
+        }
+    }
+    SetPosition(size:number) {
+        this.image.setScale(size); 
+        if (size == 1) {
+            this.image.x = this.fullSizeX;
+            this.image.y = this.fullSizeY;
+            this.image.displayWidth = this.fullSizeOffsetWidth;
+            this.image.displayHeight = this.fullSizeOffsetHeight;
+        }
+        else if (size == 0.5) {
+            this.image.x = this.SelectedSizeX;
+            this.image.y = this.SelectedSizeY;
+        }
+    }
 
+    private handleClick(pointer: Phaser.Input.Pointer) {
+        if (pointer.leftButtonDown()) {
+            this.toggleSelected();
+        }
+    }
+
+    private handleKeyDown(event: KeyboardEvent) {
+        if (event.key === this.hotkey) {
+            this.toggleSelected();
+        }
+    }
+
+    private MouseOver() {
+        if (!this.selected && !this.SomeSelected) {
+        this.SetPosition(0.5);
+        }
+    }
+
+    private MouseOut() {
+        if (!this.selected) {
+        this.SetPosition(1);
+        }
+    }
+
+    SomeSelectedCheck(CheckSomeSelected:boolean){
+        if (CheckSomeSelected == true){
+            this.SomeSelected = true;
+        }
+        else {
+            this.SomeSelected = false;
+        }
+    }
+
+    private toggleSelected() {
+        if (!this.selected && !this.SomeSelected) {
+            this.SetPosition(0.5);
+            this.selected = true;
+        } else {
+            this.SetPosition(1);
+            this.selected = false;
+        }
     }
 }
 
--- src/game/scenes/Inventory.ts
+++ src/game/scenes/Inventory.ts
@@ -18,39 +18,28 @@ export class Inventory extends Scene {
         const canvasHeight = this.sys.game.canvas.height;
         const inventoryWidth = 1000;
         const inventoryHeight = 180;
-
         const PlayerColor = 0xffe200; // брать из БД
-
-        // TODO
-        const axe = new Resource(this, ResourceType.AXE, );
-        
-
-        // Заглобалить
-        let Axe;
-        let Shovel;
-        let Pickaxe;
-        let Spear;
-        let Rope;
-        let Rom;
-        let Rom_Choise = false;
-        let Rope_Choise = false;
-        let Spear_Choise = false;
-        let Axe_Choise = false;
-        let Shovel_Choise = false;
-        let Pickaxe_Choise = false;
 
         // Позиция прямоугольника инвентаря
         const inventoryX = (canvasWidth - inventoryWidth) / 2; // Вычисляем X-координату для центрирования
         const inventoryY = canvasHeight - inventoryHeight; // Фиксированная Y-координата, чтобы прямоугольник находился внизу
-    
+
+        const cellWidth = inventoryWidth / 5;
+        const cellHeight = inventoryHeight / 3;
+        // TODO
+        const spear = new Resource(this, ResourceType.SPEAR, "1", inventoryX+10, inventoryY+10, cellWidth*4-50, 40,inventoryX+220,inventoryY+20);
+        const axe = new Resource(this, ResourceType.AXE, "2", inventoryX+5, inventoryY+65, cellWidth - 10, 110,inventoryX+55,inventoryY+90);
+        const pickaxe = new Resource(this, ResourceType.PICKAXE, "3", inventoryX+205, inventoryY+65, cellWidth - 10, 110,inventoryX+270,inventoryY+85);
+        const shovel = new Resource(this, ResourceType.SHOVEL, "4", inventoryX+405, inventoryY+65, cellWidth - 10, 110,inventoryX+460,inventoryY+85);
+        const rope = new Resource(this, ResourceType.ROPE, "5", inventoryX+615, inventoryY+65, cellWidth - 30, cellHeight - 10,inventoryX+660,inventoryY+75);
+        const rum = new Resource(this, ResourceType.RUM, "6", inventoryX+605, inventoryY+125, cellWidth - 10, cellHeight - 10 ,inventoryX+660,inventoryY+135);
+
         // Создаем прямоугольник инвентаря
         const inventoryRect = this.add.rectangle(inventoryX, inventoryY, inventoryWidth, inventoryHeight, PlayerColor);
         inventoryRect.setOrigin(0);
         inventoryRect.setAlpha(0.3);
-    
+       
         // Рисуем горизонтальные линии таблицы
-        const cellWidth = inventoryWidth / 5;
-        const cellHeight = inventoryHeight / 3;
         for (let i = 1; i < 3; i++) {
             const y = inventoryY + i * cellHeight;
             const line = this.add.graphics();
@@ -93,342 +82,70 @@ export class Inventory extends Scene {
         // Добавляем нумерацию ячеек
         let cellNumber = 1;
         let Num = 1;
-        let AX = 0;
-        let AY = 0;
-        let PX = 0;
-        let PY = 0;
-        let RX = 0;
-        let RY = 0;
-        let RomX = 0;
-        let RomY = 0;
-        let ShX = 0;
-        let ShY = 0;
-        let SpX = 0;
-        let SpY = 0;
         for (let row = 0; row < 3; row++) {
             for (let col = 0; col < 5; col++) {
-                const cellX = inventoryX + col * cellWidth + 5; // Отступ слева
-                const cellY = inventoryY + row * cellHeight + 5; // Отступ сверху
+                const cellX = inventoryX + col * cellWidth + 5;
+                const cellY = inventoryY + row * cellHeight + 5;
                 if ([1, 6, 7, 8, 9, 14].includes(cellNumber)) {
                     this.add.text(cellX, cellY, Num.toString(), { color: '#000000', fontFamily:'sans-serif', fontSize: '20px' });
                     Num++;
                     if (cellNumber === 1) {
-                        Spear = this.add.image(cellX+20, cellY+7, 'Spear');
-                        SpX = cellX+20;
-                        SpY = cellY+7;
-                        Spear.setOrigin(0);
-                        Spear.setInteractive();
-                        Spear.displayWidth = cellWidth*4-50;
-                        Spear.displayHeight = 40;
+                        spear.initialize('Spear');
+                        spear.SetPosition(1);
                     }
                     if (cellNumber === 6) {
-                        Axe = this.add.image(cellX, cellY, 'Axe');
-                        AX = cellX;
-                        AY = cellY;
-                        Axe.setOrigin(0);
-                        Axe.setInteractive();
-                        Axe.displayWidth = cellWidth - 10;
-                        Axe.displayHeight = 110;
+                        axe.initialize('Axe');
+                        axe.SetPosition(1);
                     }
                     else if (cellNumber === 7) {
-                        Pickaxe = this.add.image(cellX, cellY, 'Pickaxe');
-                        PX = cellX;
-                        PY = cellY;
-                        Pickaxe.setOrigin(0);
-                        Pickaxe.setInteractive();
-                        Pickaxe.displayWidth = cellWidth - 10; 
-                        Pickaxe.displayHeight = 110; 
+                        pickaxe.initialize('Pickaxe');
+                        pickaxe.SetPosition(1);
                     }
                     else if (cellNumber === 8) {
-                        Shovel = this.add.image(cellX, cellY, 'Shovel');
-                        ShX = cellX;
-                        ShY = cellY;
-                        Shovel.setOrigin(0);
-                        Shovel.setInteractive();
-                        Shovel.displayWidth = cellWidth - 10; 
-                        Shovel.displayHeight = 110; 
+                        shovel.initialize('Shovel');
+                        shovel.SetPosition(1);
                     }
                     else if (cellNumber === 9) {
-                        Rope = this.add.image(cellX+15, cellY, 'Rope');
-                        RX = cellX+15;
-                        RY = cellY;
-                        Rope.setOrigin(0);
-                        Rope.setInteractive();
-                        Rope.displayWidth = cellWidth - 30; 
-                        Rope.displayHeight = cellHeight - 10; 
+                        rope.initialize('Rope');
+                        rope.SetPosition(1);
                     }
                     else if (cellNumber === 14) {
-                        Rom = this.add.image(cellX+15, cellY, 'Rom');
-                        RomX = cellX+15;
-                        RomY = cellY;
-                        Rom.setOrigin(0);
-                        Rom.setInteractive();
-                        Rom.displayWidth = cellWidth - 30; 
-                        Rom.displayHeight = cellHeight - 10; 
+                        rum.initialize('Rom');
+                        rum.SetPosition(1);
                     }
                 }
                 cellNumber++;
-                
             }
         }
-
-        if (Axe) {
-            Axe.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                Axe.setScale(0.5); // Установка масштаба в 0.5 относительно оригинального размера
-                Axe.x = inventoryX+50;
-                Axe.y = inventoryY+90;
-                }
-            });
-            Axe.on('pointerout', () => {
-                if (Axe_Choise === false){
-                    Axe.setScale(1); 
-                    Axe.x = AX;
-                    Axe.y = AY;
-                    Axe.displayWidth = cellWidth - 10;
-                    Axe.displayHeight = 110;
-                }
-            });
-            Axe.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Axe_Choise = true;
-                }
-                else {
-                    Axe_Choise = false;
-                }
-            }); 
-        }
-        if (Pickaxe) {
-            Pickaxe.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                    Pickaxe.setScale(0.5); 
-                    Pickaxe.x = inventoryX+270;
-                    Pickaxe.y = inventoryY+85;
-                }
-            });
-            Pickaxe.on('pointerout', () => {
-                if (Pickaxe_Choise === false){
-                    Pickaxe.setScale(1); 
-                    Pickaxe.x = PX;
-                    Pickaxe.y = PY;
-                    Pickaxe.displayWidth = cellWidth - 10;
-                    Pickaxe.displayHeight = 110;
-                }
-            });
-            Pickaxe.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Pickaxe_Choise = true;
-                }
-                else {
-                    Pickaxe_Choise = false;
-                }
-            });    
-        }
-        if (Shovel) {
-            Shovel.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                    Shovel.setScale(0.5);
-                    Shovel.x = inventoryX+460;
-                    Shovel.y = inventoryY+85;
-                }
-            });
-            Shovel.on('pointerout', () => {
-                if (Shovel_Choise === false){
-                    Shovel.setScale(1); 
-                    Shovel.x = ShX;
-                    Shovel.y = ShY;
-                    Shovel.displayWidth = cellWidth - 10;
-                    Shovel.displayHeight = 110;
-                }
-            });
-            Shovel.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Shovel_Choise = true;
-                }
-                else {
-                    Shovel_Choise = false;
-                }
-            });    
-        }
-        if (Spear) {
-            Spear.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                    Spear.setScale(0.5); 
-                    Spear.x = inventoryX+220;
-                    Spear.y = inventoryY+20;
-                }
-            });
-            Spear.on('pointerout', () => {
-                if (Spear_Choise === false){
-                    Spear.setScale(1); 
-                    Spear.x = SpX;
-                    Spear.y = SpY;
-                    Spear.displayWidth = cellWidth*4-50;
-                    Spear.displayHeight = 40;
-                }
-            });
-            Spear.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Spear_Choise = true;
-                }
-                else {
-                    Spear_Choise = false;
-                }
-            });    
-        }
-        if (Rope) {
-            Rope.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                    Rope.setScale(0.5);
-                    Rope.x = inventoryX+660;
-                    Rope.y = inventoryY+75;
-                }
-            });
-            Rope.on('pointerout', () => {
-                if (Rope_Choise === false){
-                    Rope.setScale(1); 
-                    Rope.x = RX;
-                    Rope.y = RY;
-                    Rope.displayWidth = cellWidth - 30; 
-                    Rope.displayHeight = cellHeight - 10;
-                }
-            });
-            Rope.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Rope_Choise = true;
-                }
-                else {
-                    Rope_Choise = false;
-                }
-            });    
-        }
-        if (Rom) {
-            Rom.on('pointerover', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                    Rom.setScale(0.5); 
-                    Rom.x = inventoryX+660;
-                    Rom.y = inventoryY+135;
-                }
-            });
-            Rom.on('pointerout', () => {
-                if (Rom_Choise === false){
-                    Rom.setScale(1); 
-                    Rom.x = RomX;
-                    Rom.y = RomY;
-                    Rom.displayWidth = cellWidth - 30; 
-                    Rom.displayHeight = cellHeight - 10;
-                }
-            });
-            Rom.on('pointerdown', () => {
-                if (Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false){
-                    Rom_Choise = true;
-                }
-                else {
-                    Rom_Choise = false;
-                }
-            });    
-        }
-
-        if (Spear && Axe && Pickaxe && Shovel && Rope && Rom){
-        document.addEventListener('keydown', (event) => {
-            if (event.key === '1' && Shovel_Choise === false && Pickaxe_Choise === false && Rope_Choise === false && Axe_Choise === false && Rom_Choise === false) {
-                if (Spear_Choise === true){
-                    Spear_Choise = false;
-                    Spear.setScale(1); 
-                    Spear.x = SpX;
-                    Spear.y = SpY;
-                    Spear.displayWidth = cellWidth*4-50;
-                    Spear.displayHeight = 40;
-                }
-                else {
-                    Spear_Choise = true;
-                    Spear.setScale(0.5); 
-                    Spear.x = inventoryX+220;
-                    Spear.y = inventoryY+20;
-                }
+        let SomeSelected;
+        document.addEventListener("keydown", function(event) {
+            if(!axe.selected && !spear.selected && !pickaxe.selected && !shovel.selected && !rope.selected && !rum.selected) {
+                SomeSelected = false;
             }
-            if (event.key === '2' && Shovel_Choise === false && Pickaxe_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                if (Axe_Choise === true){
-                    Axe_Choise = false;
-                    Axe.setScale(1); 
-                    Axe.x = AX;
-                    Axe.y = AY;
-                    Axe.displayWidth = cellWidth - 10;
-                    Axe.displayHeight = 110;
-                }
-                else {
-                    Axe_Choise = true;
-                    Axe.setScale(0.5);
-                    Axe.x = inventoryX+50;
-                    Axe.y = inventoryY+90;
-                }
+            else {
+                SomeSelected = true;
             }
-            if (event.key === '3' && Axe_Choise === false && Shovel_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                if (Pickaxe_Choise === true){
-                    Pickaxe_Choise = false;
-                    Pickaxe.setScale(1); 
-                    Pickaxe.x = PX;
-                    Pickaxe.y = PY;
-                    Pickaxe.displayWidth = cellWidth - 10;
-                    Pickaxe.displayHeight = 110;
-                }
-                else {
-                    Pickaxe_Choise = true;
-                    Pickaxe.setScale(0.5);
-                    Pickaxe.x = inventoryX+270;
-                    Pickaxe.y = inventoryY+85;
-                }
-            }
-            if (event.key === '4' && Pickaxe_Choise === false && Axe_Choise === false && Rope_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                if (Shovel_Choise === true){
-                    Shovel_Choise = false;
-                    Shovel.setScale(1); 
-                    Shovel.x = ShX;
-                    Shovel.y = ShY;
-                    Shovel.displayWidth = cellWidth - 10;
-                    Shovel.displayHeight = 110;
-                }
-                else {
-                    Shovel_Choise = true;
-                    Shovel.setScale(0.5);
-                    Shovel.x = inventoryX+460;
-                    Shovel.y = inventoryY+85;
-                }
-            }
-            if (event.key === '5' && Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Spear_Choise === false && Rom_Choise === false) {
-                if (Rope_Choise === true){
-                    Rope_Choise = false;
-                    Rope.setScale(1); 
-                    Rope.x = RX;
-                    Rope.y = RY;
-                    Rope.displayWidth = cellWidth - 30; 
-                    Rope.displayHeight = cellHeight - 10;
-                }
-                else {
-                    Rope_Choise = true;
-                    Rope.setScale(0.5);
-                    Rope.x = inventoryX+660;
-                    Rope.y = inventoryY+75;
-                }
-            }
-            if (event.key === '6' && Pickaxe_Choise === false && Axe_Choise === false && Shovel_Choise === false && Spear_Choise === false && Rope_Choise === false) {
-                if (Rom_Choise === true){
-                    Rom_Choise = false;
-                    Rom.setScale(1); 
-                    Rom.x = RomX;
-                    Rom.y = RomY;
-                    Rom.displayWidth = cellWidth - 30; 
-                    Rom.displayHeight = cellHeight - 10;
-                }
-                else {
-                    Rom_Choise = true;
-                    Rom.setScale(0.5); 
-                    Rom.x = inventoryX+660;
-                    Rom.y = inventoryY+135;
-                }
-            }
+            spear.SomeSelectedCheck(SomeSelected);
+            axe.SomeSelectedCheck(SomeSelected);
+            pickaxe.SomeSelectedCheck(SomeSelected);
+            shovel.SomeSelectedCheck(SomeSelected);
+            rope.SomeSelectedCheck(SomeSelected);
+            rum.SomeSelectedCheck(SomeSelected);
         });
-    }
+        document.addEventListener("mousedown", function(event) {
+            if(!axe.selected && !spear.selected && !pickaxe.selected && !shovel.selected && !rope.selected && !rum.selected) {
+                SomeSelected = false;
+            }
+            else {
+                SomeSelected = true;
+            }
+            spear.SomeSelectedCheck(SomeSelected);
+            axe.SomeSelectedCheck(SomeSelected);
+            pickaxe.SomeSelectedCheck(SomeSelected);
+            shovel.SomeSelectedCheck(SomeSelected);
+            rope.SomeSelectedCheck(SomeSelected);
+            rum.SomeSelectedCheck(SomeSelected);
+          });
         EventBus.emit('current-scene-ready', this);
 
         // Get data from API
@@ -438,5 +155,8 @@ export class Inventory extends Scene {
                 console.log(`${element.resource.name}: ${element.amount}`);
             });
         })();
+    }
+    EventStart(){
+        
     }
 }
